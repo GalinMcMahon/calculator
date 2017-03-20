2528b45499faea11f9f56618d43beeca7b652507
Galin McMahon and Jennifer Kinsey <galin@>
2017-03-20 10:26:36 -0700

    Add multiple math functions and an alert with the results of each.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,28 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+
+var addResult = add(number1, number2);
+var subtractResult = subtract(number1, number2);
+var multResult = multiply(number1, number2);
+var divResult = divide(number1, number2);
+
+alert("Your result is: \n" +
+"Adding your numbers: " + addResult + "\n" +
+"Subtracting your numbers: " + subtractResult +
+"\n Multiplying your numbers: " + multResult +
+"\n Dividing your numbers: " + divResult); 
